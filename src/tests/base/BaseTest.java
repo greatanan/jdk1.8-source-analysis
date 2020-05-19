4cb8da5d891c8c59d2b21c48c0f282b1247666ef
郭传雷 <chuanlei.guo@baozun.com>
2020-05-19 11:17:57 +0800

    Initial commit

---
+++ src/tests/base/BaseTest.java
@@ -0,0 +1,8 @@
+package tests.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseTest {
+    protected Logger log = LoggerFactory.getLogger(this.getClass());
+}
---
+++ src/tests/java/lang/HashMapTest.java
@@ -0,0 +1,20 @@
+package tests.java.lang;
+
+import org.junit.Test;
+import tests.base.BaseTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: chuanlei.guo
+ * @Date: 2020-05-19
+ */
+public class HashMapTest extends BaseTest {
+    @Test
+    public void testHashcode() {
+        Map<String,String> map = new HashMap<>();
+        map.put("test", "test");
+    }
+}
